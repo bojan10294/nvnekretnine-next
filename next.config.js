5de064d3d1ea84059c4684410949590bbc8a9dcf
bojan_petkovic <bojan.petkovic@asioso.com>
2026-03-25 18:16:31 +0100

    Update image domains in next.config.js and upgrade sharp package version

--- next.config.js
+++ next.config.js
@@ -1,12 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['http://localhost', '127.0.0.1', 
-              'https://clownfish-app-hrroz.ondigitalocean.app', 
-              'https://hammerhead-app-ko42k.ondigitalocean.app', 
-              'clownfish-app-hrroz.ondigitalocean.app', 
-              'https://nvnekretnine.rs',
-              'res.cloudinary.com']
+    domains: [
+      'localhost',
+      '127.0.0.1',
+      'clownfish-app-hrroz.ondigitalocean.app',
+      'hammerhead-app-ko42k.ondigitalocean.app',
+      'nvnekretnine.rs',
+      'res.cloudinary.com',
+    ],
   },
   output: 'standalone',
   reactStrictMode: true
