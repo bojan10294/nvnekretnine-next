211699041a71c3a67b8fe12b6bd200558eb65a0c
Bojan Petkovic <43894182+bojan10294@users.noreply.github.com>
2026-03-25 17:52:06 +0100

    Configure Cloudinary as image loader

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    loader: 'cloudinary',
+    path: 'https://res.cloudinary.com/dry6zqlis/image/upload/',
     domains: ['http://localhost', '127.0.0.1', 'https://clownfish-app-hrroz.ondigitalocean.app', 'https://hammerhead-app-ko42k.ondigitalocean.app', 'clownfish-app-hrroz.ondigitalocean.app', 'res.cloudinary.com']
   },
   output: 'standalone',
